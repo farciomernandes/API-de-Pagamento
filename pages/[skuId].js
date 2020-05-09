bbd1fd4f9f3fb85b9052a693084b4a832acb5ddd
Marcio Fernandes Dos Santos <farciomernandes@gmail.com>
2020-05-09 15:18:53 -0300

    API integrada ao STRIPE

---
+++ pages/[skuId].js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Link from 'next/link';
+import Stripe from 'stripe';
+
+
+import stripeConfig from '../config/stripe';
+
+export async function getStaticPaths(){ //Função que realiza a busca de dados no stripe
+    const stripe = new Stripe(stripeConfig.secretKey, { //Usa a chave secreta de stripe
+        apiVersion:'2020-03-02'
+    });
+
+    const skus = await stripe.skus.list() //Recebe um array com a lista de produtos 
+
+
+    const paths = skus.data.map((sku) =>({ //Para cada produto recebido execute a função
+        params:{
+            skuId: sku.id, //Retorne o valor do ID dos produtos para skuId (http atual vira um array de ids)
+        } ,
+    }))
+
+
+    return{ //Retorno obrigatório com a tipagem
+        paths,
+        fallback:false
+    };
+}
+
+export const getStaticProps = async ( {params} )=>{ //Conteúdo retornado para a página após a consulta
+    const stripe = new Stripe(stripeConfig.secretKey, { //Usa a chave secreta de stripe
+        apiVersion:'2020-03-02'
+    });
+
+    const { skuId } = params;
+
+    const sku = await stripe.skus.retrieve(skuId); //Recebe o data do SKU referente ao ID passado no params
+     
+
+
+    return{ 
+        props: { //Props é o conteúdo propriamente dito para a página
+            sku
+        } 
+    }
+}
+
+ 
+
+function Product({ sku }){ //Função de retorno utilizando as infos
+    return( 
+        <div>
+            <h2>{sku.attributes.name}</h2>
+
+            {sku.image && <img src={sku.image}
+            style={{
+                width:'200px'
+            }} />}
+
+            <h3>{Number(sku.price/100).toFixed(2)} {sku.currency.toUpperCase()}</h3>
+            <Link href="/"> Voltar </Link>
+        </div>
+    )
+}
+
+export default Product;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
-import Head from 'next/head'
+import Stripe from 'stripe';
+import Link from 'next/link';
+import stripeConfig from '../config/stripe';
 
-export default function Home() {
-  return (
-  <div>Marcio Fernandes</div>      
+export async function getStaticProps(){ //Função que realiza a busca de dados no stripe
+  const stripe = new Stripe(stripeConfig.secretKey, { //Usa a chave secreta de stripe
+      apiVersion:'2020-03-02'
+  });
+
+  const skus = await stripe.skus.list() //Recebe um array com a lista de produtos 
+
+  return{ //Retorna todos os produtos recebidos na lista
+      props:{
+        skus: skus.data //Uso de data para pegar apenas as informações importantes para o uso
+      }
+  };
+}
+
+function Home({ skus }) {
+  console.log(skus)
+  return ( // <> = Fragment
+    <> 
+      <h1>Simple Stripe Store</h1>
+
+      {skus.map(sku =>( //Map retornando uma JSX
+        <div>
+           <h2>{sku.attributes.name}</h2>
+
+            {sku.image && <img src={sku.image}
+            style={{
+            width:'200px'
+            }} />}
+
+            <h3>{Number(sku.price/100).toFixed(2)} {sku.currency.toUpperCase()}</h3>
+            <Link href={`/${sku.id}`}> Ver item </Link>
+
+
+          <hr />
+        </div>
+      ))}
+     
+    </>    
   )
 }
+
+export default Home;
